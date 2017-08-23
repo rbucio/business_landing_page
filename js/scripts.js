9260af199c6c2e967a419918f1669390b648d4a4
Rey <rbuciodev@gmail.com>
2017-08-23 02:15:33 -0700

    replace images and added responsive media queries

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // animate on scroll
     new WOW().init();
 
-    //magnific-popup
+    // magnific-popup
     $('#work').magnificPopup({
         delegate: 'a',
         type: 'image',
@@ -11,16 +11,27 @@ $(document).ready(function() {
         }
     })
 
-    //owl carousel for team section
+    // owl carousel for team section
     $('#team-members').owlCarousel({
         items: 3,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsive: {
+            0 : {
+                items: 1
+            },
+            480 : {
+                items: 2
+            },
+            768 : {
+                items: 3
+            }
+        }
     });
 
-    //owl carousel for testimonials
+    // owl carousel for testimonials
     $('#customers-testimonials').owlCarousel({
         items: 1,
         autoplay: true,
@@ -29,16 +40,16 @@ $(document).ready(function() {
         autoplayHoverPause: true
     });
 
-    //smooth scrolling
+    // smooth scrolling
     $('a.smooth-scroll').click( function(e){
         e.preventDefault();
         var section = $(this).attr('href');
         $('html, body').animate({
-            scrollTop: $(section).offset().top - 55
+            scrollTop: $(section).offset().top - 65
         }, 1200);
     });
 
-    //hide show back to top button
+    // hide/show back to top button
     $(window).scroll(function(){
         if ($(this).scrollTop() < 250) {
             //hide button
@@ -46,6 +57,11 @@ $(document).ready(function() {
         } else {
             $('#back-to-top').fadeIn();
         }
+    })
+
+    // toggle mobile nav on item click
+    $('.navbar-collapse ul li a').on('click touch', function(){
+        $('.navbar-toggle').click();
     })
 
 });
